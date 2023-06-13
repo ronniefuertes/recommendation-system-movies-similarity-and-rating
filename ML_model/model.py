56ce82bb87b06f7f92ea8e79ebf2f329687dccb3
Ronnie <ronnie_f@outlook.com>
2023-06-13 13:24:38 -0500

    Latest update
    Updating ML model in API

---
+++ ML_model/model.py
@@ -0,0 +1,63 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+def movie_recommendation(title: str) -> dict:
+    """Check for the movie realese and popularity."""
+
+    info = {"movie_recommendations": []}
+
+    movies = pd.read_csv(r"ML_model\data\overview.csv")
+    # Get the number of rows in the DataFrame
+    num_rows = len(movies)
+
+    # Set the desired number of rows (half of the original)
+    desired_num_rows = num_rows // 4
+
+    # Randomly select a subset of rows
+    movies = movies.sample(n=desired_num_rows, random_state=2)
+
+    cv=CountVectorizer(max_features=5000, stop_words='english')
+
+    vector=cv.fit_transform(movies['overview'].values.astype('U')).toarray()
+
+    similarity=cosine_similarity(vector)
+
+    matching_rows = movies[movies['title'] == title]
+
+    # if len(matching_rows) > 0:
+    #     movie_index = matching_rows.index[0]
+    #     # Further operations using movie_index
+    # else:
+    #     print("No movie found with the title 'Iron Man'")
+    recomendation = []
+
+    distance = sorted(list(enumerate(similarity[2])), reverse=True, key=lambda vector:vector[1])
+    for i in distance[0:5]:
+        name = movies.iloc[i[0]].title
+        recomendation.append(name)
+    
+    info["movie_recommendations"] = recomendation
+
+    return info
+
+
+## For future improvement of the system
+# def belongs_to_collection(title):
+#     """Verify if the movie belongs to a movies series."""
+#     title_normalized = normalize_string(title)
+
+#     dataframe = read_movies_data(path1)
+    
+#     id_movie = []
+
+#     for row in dataframe:
+#         movie = row["title"]
+#         movie_normalized = normalize_string(movie)
+
+#         if movie_normalized == title_normalized:
+#             idx_movie = int(row["id"])
+#             id_movie.append(idx_movie)
+
+#     return idx_movie
--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 """API para extraer información de la base de datos de películas"""
 
 from fastapi import FastAPI
-from api.utils.helpers import count_movies_released_month
-from api.utils.helpers import count_movies_released_day
-from api.utils.helpers import movie_popularity
-from api.utils.helpers import movie_vote
-from api.utils.helpers import actor_info
-from api.utils.helpers import director_info
+from api.utils.helpers import director_info, count_movies_released_day
+from api.utils.helpers import actor_info, count_movies_released_month
+from api.utils.helpers import movie_popularity, movie_vote
+from ML_model.model import movie_recommendation
 
 app_description = """
-        Los títulos de películas, los nombres de actores y directores deben ir separados por '-'.
+        Los títulos de películas, los nombres de actores y directores pueden ir separados con espacio o '-'.
         No importa si está en mayusculas o minusculas, o si la vocal posee tilde.
         """
 
@@ -41,7 +39,7 @@ def cantidad_filmaciones_dia(dia:str):
 def score_titulo(titulo:str):
     """
     Ingresa el título de una filmación para ver el año de estreno y su calificación.
-    Los espacios en el título de la película deben ser reemplazados por '-'.
+    Los espacios en el título de la película también pueden ir separados con '-'.
     No importa si está en mayusculas o minusculas, o si la vocal posee tilde.
     """
     info = movie_popularity(titulo)
@@ -54,7 +52,7 @@ def votos_titulo(titulo:str):
     Ingresa el título de una filmación para ver la cantidad de votos y el valor promedio de las votaciones.
     Si la cantidad de votos es menor a 2000, aparece un mensaje avisando que no cumple esta condición.
     
-    Los espacios en el título de la película deben ser reemplazados por '-'.
+    Los espacios en el título de la película también pueden ir separados con '-'.
     No importa si está en mayusculas o minusculas, o si la vocal posee tilde.
     """
     info = movie_vote(titulo)
@@ -67,7 +65,7 @@ def get_actor(nombre_actor:str):
     Ingresa el nombre de un actor para ver el éxito de él medido a través del retorno.
     Adicional, se muestra la cantidad de películas que en las que ha participado y el promedio de retorno.
 
-    Los espacios en el nombre del actor deben ser reemplazados por '-'.
+    Los espacios en el nombre del actor también pueden ir separados con '-'.
     No importa si está en mayusculas o minusculas, o si la vocal posee tilde.
 
     """
@@ -82,10 +80,20 @@ def get_director(nombre_director:str):
     Adicional, se muestra el nombre de cada película que ha dirigido, su la fecha de lanzamiento, 
         el retorno de cada película, su costo y la ganancia de la misma.
 
-    Los espacios en el nombre del actor deben ser reemplazados por '-'.
+    Los espacios en el nombre del director también pueden ir separados con '-'.
     No importa si está en mayusculas o minusculas, o si la vocal posee tilde.
     """
     info = director_info(nombre_director)
     return {'director':info["name"], 'retorno_total_director':info["return_total"], 'peliculas':info["movies_total"], 
             'anio':info["release_date"], 'retorno_pelicula':info["return_movie"], 'budget_pelicula':info["budget_movie"],
             'revenue_pelicula':info["revenue_movie"]}
+
+
+# ML
+@app.get('/recomendacion/{titulo}')
+def recomendacion(titulo:str):
+    '''
+    Ingresa el nombre de una pelicula para ver 5 películas similares.
+    '''
+    info = movie_recommendation(titulo)
+    return {'lista recomendada': info["movie_recommendations"]}
